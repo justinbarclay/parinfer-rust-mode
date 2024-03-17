eb4b012a1b9478d06b2349e8aaa658307a3501a9
Justin Barclay <git@justinbarclay.ca>
2024-03-17 16:26:54 -0700

    Add flake support

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "A very basic flake";
+  inputs =
+    {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+      flake-utils.url = "github:numtide/flake-utils";
+    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            # self.overlays.default
+          ];
+        };
+
+        lib = nixpkgs.lib;
+
+        parinfer-rust-mode = pkgs.emacsPackages.trivialBuild
+          {
+            pname = "parinfer-rust-mode";
+            version = "0.1.0";
+            src = ./.;
+            nativeBuildInputs = with pkgs; [ emacsPackages.cask python3 which ];
+            checkPhase = ''
+              echo "Running tests"
+              PARINFER_RUST_TEST=true
+              make test
+            '';
+            doCheck = true;
+            meta = {
+              description = "An Emacs interface for the parinfer-rust library";
+              license = lib.licenses.gpl3;
+              platforms = lib.platforms.all;
+            };
+          };
+      in
+      {
+        packages = {
+          default = parinfer-rust-mode;
+        };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs;
+            [ emacsPackages.cask python3 ];
+        };
+
+      });
+}
